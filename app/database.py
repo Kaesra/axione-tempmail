df38de1932bbd82b75c28cbe98b1402be8f579eb
Kaesra <scutieeop@gmail.com>
2026-03-25 10:27:55 +0300

    Add approval auth and richer HTML parsing for app/database.py

--- app/database.py
+++ app/database.py
@@ -12,7 +12,7 @@ class Base(DeclarativeBase):
 
 connect_args = {"check_same_thread": False} if settings.db_url.startswith("sqlite") else {}
 engine = create_engine(settings.db_url, connect_args=connect_args)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
 
 
 def init_db() -> None:
